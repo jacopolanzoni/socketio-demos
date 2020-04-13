c38d4b60555051b1b664e8f2902903cad2f85581
jacopolanzoni <jacopolanzoni@gmail.com>
2020-04-13 19:20:30 +0100

    Add basic on/emit functionality between client and server.

--- btcApp_before/app.js
+++ btcApp_before/app.js
@@ -1,12 +1,27 @@
 var express = require('express');
+var http = require('http');
+var socketIo = require('socket.io');
 var path = require('path');
+
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var server = http.Server(app);
+var io = socketIo(server);
 var port = 8080;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-server.listen(port, function() {
-  console.log("Listening on port " + port);
+io.on('connection', (socket) => {
+  console.log(`New connection made`);
+
+  socket.emit('message-from-server', {
+    greeting: 'Hello from Server'
+  });
+
+  socket.on('message-from-client', (message) => {
+    console.log(message);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
